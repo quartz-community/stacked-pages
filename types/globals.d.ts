7344a7b9ffe5d4dfcee30025046523cc5704dc6c
Emile Bangma <github@emilebangma.com>
2026-03-13 18:57:52 +0100

    Initial commit

---
+++ types/globals.d.ts
@@ -0,0 +1,21 @@
+interface CustomEventMap {
+  nav: CustomEvent<{ url: string }>;
+  prenav: CustomEvent<undefined>;
+  themechange: CustomEvent<{ theme: "light" | "dark" }>;
+}
+
+interface Document {
+  addEventListener<K extends keyof CustomEventMap>(
+    type: K,
+    listener: (this: Document, ev: CustomEventMap[K]) => void,
+  ): void;
+  removeEventListener<K extends keyof CustomEventMap>(
+    type: K,
+    listener: (this: Document, ev: CustomEventMap[K]) => void,
+  ): void;
+  dispatchEvent<K extends keyof CustomEventMap>(ev: CustomEventMap[K] | UIEvent): void;
+}
+
+interface Window {
+  addCleanup(fn: (...args: unknown[]) => void): void;
+}
